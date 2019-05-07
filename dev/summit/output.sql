8afa30d7a4b0860d487373d23bea9df638e1ffd1
meta-morpho-sys <yuliya.nedyalkova777@gmail.com>
2019-05-07 23:45:49 +0100

    Cleanup

--- dev/summit/output.sql
+++
@@ -1,9 +0,0 @@
-UPDATE ctl_batch_schedules
-SET DISABLED = 'N'
-WHERE work_station = 'WSOGL-1'
-AND schedule IN
-(-- 'THESE', -- comment 
-'are', --another comment
-'ABCS', --third comment
-'projectS' --fourth comment
-);
