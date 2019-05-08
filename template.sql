20a894c47aa49e5882497200fbd63787d90a6619
meta-morpho-sys <yuliya.nedyalkova777@gmail.com>
2019-05-08 14:54:55 +0100

    Test 7

--- template.sql
+++ template.sql
@@ -7,19 +7,5 @@ AND schedule IN
 (${env_project_names}
 );
 
-DO $$ 
 DECLARE
     P_COB_DATE  VARCHAR(200) := '${p_cob_date}';
-BEGIN
-    RAISE NOTICE 'Date Inputted by Veerraju is: %', P_COB_DATE;
-    RAISE NOTICE '%', P_COB_DATE;
-END $$;
-
-DO $$ 
-DECLARE
-   created_at time := NOW();
-BEGIN 
-   RAISE NOTICE '%', created_at;
-   PERFORM pg_sleep(10);
-   RAISE NOTICE '%', created_at;
-END $$;
