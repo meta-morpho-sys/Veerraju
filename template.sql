a2fd8da91fa50dcd7e358442776e0ea61220faff
meta-morpho-sys <yuliya.nedyalkova777@gmail.com>
2019-05-08 14:19:57 +0100

    Inserted parameter for Manually inputted date

--- template.sql
+++ template.sql
@@ -1,6 +1,15 @@
+-- UPDATE ctl_batch_schedules SET disabled = 'Y';
+
 UPDATE ctl_batch_schedules
-SET DISABLED = 'N'
+SET disabled = 'N'
 WHERE work_station = '${work_station}'
 AND schedule IN
 (${env_project_names}
 );
+
+DECLARE
+  P_COB_DATE  VARCHAR2(200)
+BEGIN
+    P_COB_DATE := '${p_cob_date}'
+    raise notice 'Date Inputted by Veerraju is : %', P_COB_DATE;
+END
