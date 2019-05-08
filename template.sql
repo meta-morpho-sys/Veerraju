e88ab99a8e8ec404d9c818f51ccb5627e967a324
meta-morpho-sys <yuliya.nedyalkova777@gmail.com>
2019-05-08 14:49:06 +0100

    Test 5

--- template.sql
+++ template.sql
@@ -12,15 +12,4 @@ DECLARE
     P_COB_DATE  VARCHAR2(200) := '${p_cob_date}';
 BEGIN
     RAISE NOTICE 'Date Inputted by Veerraju is: %', P_COB_DATE;
-END $$:
-
-
-DO $$ 
-DECLARE
-   counter    INTEGER := 1;
-   first_name VARCHAR(50) := 'John';
-   last_name  VARCHAR(50) := 'Doe';
-   payment    NUMERIC(11,2) := 20.5;
-BEGIN 
-   RAISE NOTICE '% % % has been paid % USD', counter, first_name, last_name, payment;
 END $$;
